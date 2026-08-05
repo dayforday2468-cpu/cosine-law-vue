1b87bd0f3373f021b9abcd20905c7c11accf4f30
MyeongcheolKim <dayforday2468@gmail.com>
2026-08-05 14:27:01 +0900

    refactor: simplify visualizer imports with index exports

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
-import CosineLawVisualizer from './components/visualizations/cosine-law/CosineLawVisualizer.vue'
-import AngleInvarianceVisualizer from './components/visualizations/angle-invariance/AngleInvarianceVisualizer.vue'
-import HaversineVisualizer from './components/visualizations/haversine-formula/HaversineVisualizer.vue'
+import {
+  CosineLawVisualizer,
+  AngleInvarianceVisualizer,
+  HaversineVisualizer,
+} from '@/components/visualizations'
 </script>
 
 <template>
---
+++ src/components/visualizations/index.js
@@ -0,0 +1,3 @@
+export { default as CosineLawVisualizer } from './cosine-law/CosineLawVisualizer.vue'
+export { default as AngleInvarianceVisualizer } from './angle-invariance/AngleInvarianceVisualizer.vue'
+export { default as HaversineVisualizer } from './haversine-formula/HaversineVisualizer.vue'
